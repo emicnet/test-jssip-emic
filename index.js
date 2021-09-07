7ee8240e5f44a6513b6f1789ae21081a7ab737c5
qiulang@macbook3 <qiulang2000@126.com>
2021-09-07 15:25:28 +0800

    feat:演示被叫挂机铃音提示，注意要上传mp3文件到服务器根目录

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import phonebar from 'jssip-emicnet/dist/phonebar'
 // import phonebar from '../JsSipWrap/dist/phonebar'
 localStorage.setItem('debug', 'phonebar:*')
 
-phonebar.log('正在获取用户信息。。。')
-let un = 1000
-let pwd = 'ceshi1000!'
-let switchnumber = '02160469121'
+let un = 1001
+let pwd = 'welcome123@bj'
+let switchnumber = '02160899188'
 let gid = 0
 let calloutnumber = '95588'
 let encrpyt = 'opwmRvGhOcGMXRZSp_moOQ'
@@ -22,26 +21,34 @@ let logoutTimer = -1
 
 //演示代码，登录成功后发起呼叫
 let eventCallback = {
-    register: function (res) {
+    register(res) {
         if (res.code == 200) {
             phonebar.log('登录成功')
             phonebar.log(phonebar.checkLogin())
             setTimeout(() => {
                 phonebar.log('再次检查登录状态', phonebar.checkLogin())
             }, 1000)
-
             setTimeout(() => {
-                // 登陆成功之后，可以呼出
-                // 呼外线
-                let calltype = phonebar.call(calloutnumber)
-                phonebar.log('呼' + (calltype == 2 ? '外线' : '内线'))
-                // 呼内线
-                // let calltype = phonebar.call(callinnumber)
-                // phonebar.log('呼',(calltype ==2 ? "外线":"内线"))
+                if (eventCallback.nextDemo) {
+                    //如果有指定下一步演示方法就调用它
+                    eventCallback.nextDemo()
+                } else {
+                    // 登陆成功之后，可以呼出
+                    // 呼外线
+                    let calltype = phonebar.call(calloutnumber)
+                    phonebar.log('呼' + (calltype == 2 ? '外线' : '内线'))
+                    // 呼内线
+                    // let calltype = phonebar.call(callinnumber)
+                    // phonebar.log('呼',(calltype ==2 ? "外线":"内线"))
+                }
             }, 2000)
         }
     },
-    callEvent: function (type, data) {
+    /**
+     * type 为呼出过程中的回调状态码，data为回调输入参，
+     * 详见api文档 init章节和呼叫流程图
+     */
+    callEvent(type, data) {
         switch (type) {
             case 'newPBXCall':
                 var ccNumber = data.c
@@ -160,14 +167,14 @@ let eventCallback = {
             //     phonebar.log('callback:callEvent', type, data)
         }
     },
-    kickedOffLine: function (data) {
+    kickedOffLine(data) {
         phonebar.log(data)
         phonebar.log(
             '收到下线消息，账户也会被强制下线，这时候只需要更新相应UI就可以'
         )
         phonebar.logout()
     },
-    statusChanged: (data) => {
+    statusChanged(data) {
         if (data.status == '0') {
             phonebar.log('离线')
         }
@@ -180,68 +187,109 @@ let eventCallback = {
     },
 }
 
-let call_handler = async (err, res) => {
-    if (!err) {
-        phonebar.log('获取用户信息,包含用户信息和组信息')
-        let memberInfo = res
-        phonebar.log('用户信息', memberInfo.userData)
-        for (const group of memberInfo.inGroups) {
-            phonebar.log('组信息', group)
-            //调用init需要设置,如果在多个组，登录时就取最后一个组
-            gid = group.id
-        }
-        var userData = JSON.parse(localStorage.userData)
-        var webParam = {
-            un,
-            pwd,
-            eid: userData.eid,
-            //eid: '6565' //不存在eid res.status 50008
-        }
-        var res = await phonebar.webApiHandler('getGroups', webParam)
-        if (res.status == 200) {
-            phonebar.log('获取了所有技能组:')
-            for (const group of res.returnData) {
-                //{group.id, group.eid, group.name}
-                phonebar.log(`${group.name} : ${group.id}`)
-            }
-            const gn = res.returnData[0].name
-            const gi = res.returnData[0].id
-            webParam.searchGid = gi //'1000000015'
-            webParam.length = 10
-            var res = await phonebar.webApiHandler('searchEpMembers', webParam)
-            if (res.status == 200) {
-                phonebar.log(
-                    `查询${gn} 组成员成功返回 ${res.returnData.recordsTotal} 人`,
-                    res
-                )
-                for (const member of res.returnData.data) {
-                    //会话状态   0 离线  1 空闲  2 忙碌
-                    phonebar.log(
-                        `${member.displayname} 状态 ${member.kefuStatus}`
-                    )
-                }
-            }
-        } else {
-            phonebar.log('获取技能组失败', res)
-        }
+/**
+ * 演示外呼后，被叫挂机，主叫铃音提示
+ * 只演示外呼中几个重要的回调状态
+ */
 
-        let params = {
-            un: un,
-            switchnumber: switchnumber,
-            pwd: pwd,
-            gid: gid,
-            //webrtc-dev.emicloud.com是演示服务器，实际使用请不要用它
-            socketUri: 'wss://webrtc-dev.emicloud.com:9060',
+let hangup_demo = (type, data) => {
+    switch (type) {
+        case 'calloutResponse':
+            //获取ccnumber 通话唯一标识
+            var ccNumber = data.r == 200 ? data.c : undefined
+            if (data.r != 200) {
+                phonebar.log(`响应状态${data.r}`)
+                let msg = callfailedReason[data.r]
+                msg = msg || '呼叫失败'
+                phonebar.log(msg)
+                setTimeout(() => {
+                    phonebar.log(`10秒后退出`)
+                    phonebar.logout()
+                }, 10000)
+            } else {
+                phonebar.log('服务器响应了呼叫请求', data)
+            }
+            break
+        case 'answeredPBXCall':
+            //被叫接听
+            phonebar.log('被叫已经接听，等待被叫挂机 ...')
+            break
+        case 'endPBXCall':
+            // 获取坐席状态
+            phonebar.log('通话结束，保证铃音通知坐席；20秒后结束并退出登录 ...')
+            // 实现类似 playFinishTone() 的方法，代码大致如下
+            // 同时要确保页面有相应的 audio tag，使用mp3文件也在服务器上
+            document.getElementById('yimiduduaudio').play()
+            setTimeout(() => {
+                phonebar.log('Stop play ringtone in 4 seconds')
+                document.getElementById('yimiduduaudio').pause()
+            }, 6000) //注：setTimeout 设置6秒，大致播放4秒
+
+            setTimeout(() => {
+                phonebar.log(`已过20秒，退出登录`)
+                phonebar.logout()
+            }, 10000)
+            break
+    }
+}
+
+let getGroup_demo = async (res) => {
+    let memberInfo = res
+    phonebar.log('用户信息', memberInfo.userData)
+    for (const group of memberInfo.inGroups) {
+        phonebar.log('组信息', group)
+        //调用init需要设置,如果在多个组，登录时就取最后一个组
+        gid = group.id
+    }
+    var userData = JSON.parse(localStorage.userData)
+    var webParam = {
+        un,
+        pwd,
+        eid: userData.eid,
+        //eid: '6565' //不存在eid res.status 50008
+    }
+    var res = await phonebar.webApiHandler('getGroups', webParam)
+    if (res.status == 200) {
+        phonebar.log('获取了所有技能组:')
+        for (const group of res.returnData) {
+            //{group.id, group.eid, group.name}
+            phonebar.log(`${group.name} : ${group.id}`)
         }
-        //登录易米呼叫服务器
-        let reg = phonebar.init(params, eventCallback)
-        if (reg) {
-            phonebar.log('phonebar.init 发起注册')
-        } else {
-            phonebar.log('phonebar.init 没有发起注册，参数有问题')
+        const gn = res.returnData[0].name
+        const gi = res.returnData[0].id
+        webParam.searchGid = gi //'1000000015'
+        webParam.length = 10
+        var res = await phonebar.webApiHandler('searchEpMembers', webParam)
+        if (res.status == 200) {
+            phonebar.log(
+                `查询${gn} 组成员成功返回 ${res.returnData.recordsTotal} 人`,
+                res
+            )
+            for (const member of res.returnData.data) {
+                //会话状态   0 离线  1 空闲  2 忙碌
+                phonebar.log(`${member.displayname} 状态 ${member.kefuStatus}`)
+            }
         }
     } else {
-        phonebar.log('获取用户信息失败', err)
+        phonebar.log('获取技能组失败', res)
+    }
+}
+
+let login_demo = (cb) => {
+    let params = {
+        un: un,
+        switchnumber: switchnumber,
+        pwd: pwd,
+        gid: gid,
+        //webrtc-dev.emicloud.com是演示服务器，实际使用请不要用它
+        socketUri: 'wss://webrtc-dev.emicloud.com:9060',
+    }
+    //登录易米呼叫服务器
+    let reg = phonebar.init(params, cb)
+    if (reg) {
+        phonebar.log('phonebar.init 发起注册')
+    } else {
+        phonebar.log('phonebar.init 没有发起注册，参数有问题')
     }
 }
 
@@ -269,21 +317,49 @@ function seatStatelog(state) {
     }
 }
 
-phonebar.getUser2(
-    {
-        un,
-        pwd, //密码需要加引号
-        switchnumber,
-        // callintype: 5,
-        // number: 10000,
-    },
-    call_handler
-)
+let callDemo = async (err, res) => {
+    if (!err) {
+        getGroup_demo(res)
+        phonebar.log(`获取坐席所在技能组id:${gid}`)
+        //登录成功后会发起呼叫，详见 register() 回调方法
+        login_demo(eventCallback)
+    } else {
+        phonebar.log('获取用户信息失败', err)
+    }
+}
 
-// 原有接口 getUser 调用方式不变
-// phonebar.getUser(
-//     un,
-//     pwd, //密码需要加引号
-//     switchnumber,
-//     call_handler
-// )
+function demo() {
+    let caller = document.getElementById('callernumber').value
+    if (!caller) {
+        phonebar.log(`没有输入被叫号码，拨打95588`)
+    } else {
+        //呼叫示例演示被叫挂机
+        eventCallback.callEvent = hangup_demo
+        eventCallback.nextDemo = () => {
+            phonebar.log(`呼叫一个实际号码 ${caller}，并最后让被叫挂机`)
+            phonebar.call(caller)
+        }
+        //要确保页面上有 audio tag, 同时要确保 ring.mp3 放在服务器响应位置
+    }
+    phonebar.log('正在获取用户信息。。。')
+    phonebar.getUser2(
+        {
+            un,
+            pwd, //密码需要加引号
+            switchnumber,
+            // callintype: 5,
+            // number: 10000,
+        },
+        callDemo
+    )
+    // 原有接口 getUser 调用方式不变
+    // phonebar.getUser(
+    //     un,
+    //     pwd, //密码需要加引号
+    //     switchnumber,
+    //     call_demo
+    // )
+}
+//because we use webpack we need to add it to window
+//https://stackoverflow.com/questions/35781579/basic-webpack-not-working-for-button-click-function-uncaught-reference-error
+window.demo = demo
